68a4853d530ba804d0b50d27a90e829ca329f966
Prince <prince.jain@sparxitsolutions.com>
2017-12-29 17:26:04 +0530

    First commit

---
+++ src/CalculatorServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Prince\Calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+         include __DIR__.'/routes.php';
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // register our controller
+        $this->app->make('Prince\Calculator\CalculatorController');
+        $this->loadViewsFrom(__DIR__.'/views', 'calculator');
+    }
+}
